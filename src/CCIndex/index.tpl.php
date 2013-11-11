f78dbb465d323b0ebe7a103709d76e30883a81a2
patricja <patric_jansson@spray.se>
2013-11-11 12:57:33 +0100

    Update index.tpl.php

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -9,7 +9,7 @@
 <p>You can review its source directly on github: <a href='https://github.com/patricja/wolf'>https://github.com/patricja/wolf</a></p>
 
 <h2>Installation</h2>
-<p>First you have to make the data-directory writable. This is the place where Wolf needs
+<p>First you have to create the data-directory and make it writable. This is the place where Wolf needs
 to be able to write and create files.</p>
 <blockquote>
 <code>cd wolf; mkdir site/data</code>
@@ -17,7 +17,12 @@ to be able to write and create files.</p>
 <code>chmod 777 site/data</code>
 </blockquote>
 
-<p>Second, Wolf has some modules that need to be initialised. You can do this through a
+<p>Then you have to change the .htaccess file</p>
+<blockquote>
+# Must use RewriteBase on www.student.bth.se, Rewritebase for url /~mos/test is /~mos/test/
+</blockquote>
+
+<p>Thrid, Wolf has some modules that need to be initialised. You can do this through a
 controller. Point your browser to the following link.</p>
 <blockquote>
 <a href='<?=create_url('module/install')?>'>module/install</a>
